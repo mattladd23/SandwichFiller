71932347de2ece7637d1880592c3c87a254fd522
Matt <matt-jbl@hotmail.co.uk>
2022-06-11 13:53:15 +0100

    Created footer and set up register route

--- src/routes/register.js
+++ src/routes/register.js
@@ -2,4 +2,12 @@
 const express = require('express');
 const router = express.Router();
 
+// Registration options route
+router.get('/', (req, res) => {
+    res.render('register', {
+        title: 'Register',
+        error: false
+    });
+});
+
 module.exports = router;
