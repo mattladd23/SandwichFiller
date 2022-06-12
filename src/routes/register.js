bec0151263a10942d76baa310fe617a78c101415
Matt <matt-jbl@hotmail.co.uk>
2022-06-12 14:42:45 +0100

    Connected front and backend with prepared statement for staff registration

--- src/routes/register.js
+++ src/routes/register.js
@@ -1,6 +1,17 @@
+// Env variables
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 // Instantiate express and router
 const express = require('express');
 const router = express.Router();
+
+// Import database model
+const pool = require('../config/db');
+
+// Import relevant libraries
+const bcrypt = require('bcrypt');
 
 // Registration options registration route
 router.get('/', (req, res) => {
@@ -15,13 +26,58 @@ router.get('/register/login', (req, res) => {
     res.redirect('/login');
 });
 
-// Staff registration
+// STAFF REGISTRATION
+
+// Render page
 router.get('/staff', (req, res) => {
     res.render('reg-staff', {
         title: 'Register as a member of staff',
         error: false
     });
 });
+
+// Post registration form
+
+router.post('/staff', async (req, res) => {
+
+    // Parameterise middleware and express validation
+
+    // Error handlers
+
+    // Create form input variables
+    const staffhashedPw = await bcrypt.hash(req.body.staffpassword, 10);
+    const staffUserId = Date.now().toString();
+    const staffFName = req.body.stafffname;
+    const staffLName = req.body.stafflname;
+    const staffEmail = req.body.staffemail;
+    const staffPassword = staffhashedPw;
+
+    console.log(staffUserId, staffFName, staffFName, staffEmail, staffPassword);
+
+    // Query to check existing credentials
+
+    // Query to insert user into database
+    let q = 'SET SEARCH_PATH TO sf;'
+    + 'PREPARE registerStaff(bigint, text, text, text, text) AS '
+    + 'INSERT INTO staff (user_id, f_name, l_name, email, password) '
+    + 'VALUES ($1, $2, $3, $4, $5);'
+    + `EXECUTE registerStaff(${staffUserId}, '${staffFName}', '${staffLName}', '${staffEmail}', '${staffPassword}');`
+    + 'DEALLOCATE registerStaff;'
+
+    await pool
+        .query(q)
+        .then(() => {
+            res.redirect('/login/staff');
+        })
+    .catch((e) => {
+        console.log(e);
+    })
+
+    // Create payload and jwt variables for email verification
+
+})
+
+
 
 
 // Student registration
