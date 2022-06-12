d152af27d7a043d8a31ce92c9b64e6c365df7bb6
Matt <matt-jbl@hotmail.co.uk>
2022-06-12 21:30:56 +0100

    Register student post route and front end

--- src/routes/register.js
+++ src/routes/register.js
@@ -37,7 +37,6 @@ router.get('/staff', (req, res) => {
 });
 
 // Post registration form
-
 router.post('/staff', async (req, res) => {
 
     // Parameterise middleware and express validation
@@ -75,18 +74,66 @@ router.post('/staff', async (req, res) => {
 
     // Create payload and jwt variables for email verification
 
-})
+});
 
 
 
 
-// Student registration
+// STUDENT REGISTRATION
+
+// Render page
 router.get('/student', (req, res) => {
     res.render('reg-student', {
         title: 'Register as a student',
         error:false
     });
 });
+
+// Post registration form
+router.post('/student', async (req, res) => {
+
+    // Parameterise middleware and express validation
+
+    // Error handlers
+
+    // Create form input variables
+    const studenthashedPw = await bcrypt.hash(req.body.studentpassword, 10);
+    const studentUserId = Date.now().toString();
+    const studentFName = req.body.studentfname;
+    const studentLName = req.body.studentlname;
+    const studentEmail = req.body.studentemail;
+    const studentId = req.body.studentid;
+    const course = req.body.course;
+    const school = req.body.school;
+    const placementYear = req.body.placementyear;
+    const gradYear = req.body.gradyear;
+    const prefSector = req.body.prefsector;
+    const otherSectors = req.body.othersectors;
+    const studentPassword = studenthashedPw;    
+
+    // Query to check existing credentials
+
+    // Query to insert user into database
+    let q = 'SET SEARCH_PATH TO sf;'
+    + 'PREPARE registerStudent(bigint, text, text, text, text, numeric, text, text, text, numeric, text, text) AS '
+    + 'INSERT INTO student (user_id, f_name, l_name, email, password, student_id, course, school, placement_year, grad_year, pref_sector, other_sectors) '
+    + 'VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12);'
+    + `EXECUTE registerStudent(${studentUserId}, '${studentFName}', '${studentLName}', '${studentEmail}', '${studentPassword}', ${studentId}, '${course}', '${school}', '${placementYear}', ${gradYear}, '${prefSector}', '${otherSectors}');`
+    + 'DEALLOCATE registerStudent;'
+
+    await pool
+        .query(q)
+        .then(() => {
+            res.redirect('/login/student');
+        })
+    .catch((e) => {
+        console.log(e);
+    })
+
+    // Create payload and jwt variables for email verification
+
+});
+
 
 
 module.exports = router;
