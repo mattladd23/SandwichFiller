c5c4cc33564c62315d7cfb066db729462dbf3d8b
Matt <matt-jbl@hotmail.co.uk>
2022-06-12 12:03:07 +0100

    Home navigation routes built and tested

--- src/routes/login.js
+++ src/routes/login.js
@@ -2,4 +2,28 @@
 const express = require('express');
 const router = express.Router();
 
+// Registration options login route
+router.get('/', (req, res) => {
+    res.render('login', {
+        title: 'Login',
+        error: false
+    });
+});
+
+// Staff login
+router.get('/staff', (req, res) => {
+    res.render('login-staff', {
+        title: 'Login as a member of staff',
+        error: false
+    });
+});
+
+// Student login
+router.get('/student', (req, res) => {
+    res.render('login-student', {
+        title: 'Login as a student',
+        error: false
+    });
+});
+
 module.exports = router;
--- src/routes/register.js
+++ src/routes/register.js
@@ -2,12 +2,35 @@
 const express = require('express');
 const router = express.Router();
 
-// Registration options route
+// Registration options registration route
 router.get('/', (req, res) => {
     res.render('register', {
         title: 'Register',
         error: false
     });
 });
+
+// Redirect to login page from register route
+router.get('/register/login', (req, res) => {
+    res.redirect('/login');
+});
+
+// Staff registration
+router.get('/staff', (req, res) => {
+    res.render('reg-staff', {
+        title: 'Register as a member of staff',
+        error: false
+    });
+});
+
+
+// Student registration
+router.get('/student', (req, res) => {
+    res.render('reg-student', {
+        title: 'Register as a student',
+        error:false
+    });
+});
+
 
 module.exports = router;
