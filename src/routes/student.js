e316749bc5404ebee7942e005ec98ecbf4ef6f62
Matt <matt-jbl@hotmail.co.uk>
2022-06-14 16:56:32 +0100

    All three navbars introduced and active, some styling still to be done later

--- src/routes/student.js
+++ src/routes/student.js
@@ -2,4 +2,11 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.render('student-welcome', {
+        title: 'Welcome Student',
+        error: false
+    });
+});
+
 module.exports = router;
