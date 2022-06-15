a55ec978ba7664a68a3ae3c489278ece7dc52507
Matt <matt-jbl@hotmail.co.uk>
2022-06-15 22:50:42 +0100

    Got apps to render - albeit rather messily

--- src/routes/staff.js
+++ src/routes/staff.js
@@ -17,122 +17,148 @@ router.get('/', (req, res) => {
 // Get all applications
 router.get('/applications', async (req, res) => {
 
+    let q = 'SET SEARCH_PATH TO sf;'    
+    + 'SELECT DISTINCT application.app_status FROM application;'
+
     // Import to decide whether prepared or not as will need to use string escape for prepared 
-    let prepared = false
+    let prepared = false;
+    console.log(`Prepared: ${prepared}`);
 
-    // Default query to return all posts ordered by date last updated
-    // let q = 'SET SEARCH_PATH TO sf;'
-    // + 'SELECT application.user_id, application.app_id, application.role, application.organisation, application.city '
-    // + 'application.country, application.app_date, application.deadline, application.description, application.app_status '
-    // + 'application.last_updated, users.user_id, users.f_name, users.l_name '
-    // + 'FROM application JOIN users ON post.user_id = users.user_id '
-    // + 'ORDER BY application.last_updated DESC;'
+    if (Object.keys(req.query).length === 0) {
+        // Default query to return all posts ordered by date last updated
+        // q += 'SET SEARCH_PATH TO sf;'
+        // + 'SELECT application.user_id, application.app_id, application.role, application.organisation, application.city, '
+        // + 'application.country, application.app_date, application.deadline, application.description, application.app_status, '
+        // + 'application.last_updated, users.user_id, users.f_name, users.l_name '
+        // + 'FROM application JOIN users ON post.user_id = users.user_id '
+        // + 'ORDER BY application.last_updated DESC;'
 
-    // Query without join prior to setting up user sessions for testing
-    let q = 'SET SEARCH_PATH TO sf;'
-    + 'SELECT application.user_id, application.app_id, application.role, application.organisation, application.city '
-    + 'application.country, application.app_date, application.deadline, application.description, application.app_status '
-    + 'application.last_updated '
-    + 'FROM application '
-    + 'ORDER BY application.last_updated DESC;'
+        // Query without join prior to setting up user sessions for testing
+        q += 'SET SEARCH_PATH TO sf;'
+        + 'SELECT application.user_id, application.app_id, application.role, application.organisation, application.city, '
+        + 'application.country, application.app_date, application.deadline, application.description, application.app_status, '
+        + 'application.last_updated '
+        + 'FROM application '
+        + 'ORDER BY application.last_updated DESC;'
+        console.log(q);
 
-    if (req.query.filter) {
-        if (req.query.filter === 'All applications') {
-            // q = 'SET SEARCH_PATH TO sf;'
-            // + 'SELECT application.user_id, application.app_id, application.role, application.organisation, application.city '
-            // + 'application.country, application.app_date, application.deadline, application.description, application.app_status '
-            // + 'application.last_updated, users.user_id, users.f_name, users.l_name '
-            // + 'FROM application JOIN users ON post.user_id = users.user_id '
-            // + 'ORDER BY application.last_updated DESC;'
-            
-            // Query without join prior to setting up user sessions for testing
-            q = 'SET SEARCH_PATH TO sf;'
-            + 'SELECT application.user_id, application.app_id, application.role, application.organisation, application.city '
-            + 'application.country, application.app_date, application.deadline, application.description, application.app_status '
-            + 'application.last_updated'
-            + 'FROM application '
-            + 'ORDER BY application.last_updated DESC;'
-            
-        } else {
-            prepared = true;
+    } else {
+        
 
-            // q = 'SET SEARCH_PATH TO sf;'
-            // + 'PREPARE filter(text) AS '
-            // + 'SELECT application.user_id, application.app_id, application.role, application.organisation, application.city '
-            // + 'application.country, application.app_date, application.deadline, application.description, application.app_status '
-            // + 'application.last_updated, users.user_id, users.f_name, users.l_name '
-            // + 'FROM application JOIN users ON post.user_id = users.user_id '
-            // + 'WHERE application.app_status = $1 '
-            // + 'ORDER BY application.last_updated DESC;'
-            // + `EXECUTE filter('${req.query.filter}');`
-            // + 'DEALLOCATE filter;'
-            
-            // Query without join prior to setting up user sessions for testing
-            q = 'SET SEARCH_PATH TO sf;'
-            + 'PREPARE filter(text) AS '
-            + 'SELECT application.user_id, application.app_id, application.role, application.organisation, application.city '
-            + 'application.country, application.app_date, application.deadline, application.description, application.app_status '
-            + 'application.last_updated '
-            + 'FROM application '
-            + 'WHERE application.app_status = $1 '
-            + 'ORDER BY application.last_updated DESC;'
-            + `EXECUTE filter('${req.query.filter}');`
-            + 'DEALLOCATE filter;'
-        }
-    }
+        if (req.query.filter) {
+            console.log(req.query.filter);
+            if (req.query.filter === 'All applications') {
+                console.log(req.query.filter)
+                // q = 'SET SEARCH_PATH TO sf;'
+                // + 'SELECT application.user_id, application.app_id, application.role, application.organisation, application.city, '
+                // + 'application.country, application.app_date, application.deadline, application.description, application.app_status, '
+                // + 'application.last_updated, users.user_id, users.f_name, users.l_name '
+                // + 'FROM application JOIN users ON post.user_id = users.user_id '
+                // + 'ORDER BY application.last_updated DESC;'
+                
+                // Query without join prior to setting up user sessions for testing
+                q += 'SET SEARCH_PATH TO sf;'
+                + 'SELECT application.user_id, application.app_id, application.role, application.organisation, application.city, '
+                + 'application.country, application.app_date, application.deadline, application.description, application.app_status, '
+                + 'application.last_updated '
+                + 'FROM application '
+                + 'ORDER BY application.last_updated DESC;'
+                console.log(q);
+                
+            } else {
+                prepared = true;
+                console.log(`Prepared: ${prepared}`);
+                console.log(req.query.filter);
+
+                // q = 'SET SEARCH_PATH TO sf;'
+                // + 'PREPARE filter(text) AS '
+                // + 'SELECT application.user_id, application.app_id, application.role, application.organisation, application.city, '
+                // + 'application.country, application.app_date, application.deadline, application.description, application.app_status, '
+                // + 'application.last_updated, users.user_id, users.f_name, users.l_name '
+                // + 'FROM application JOIN users ON post.user_id = users.user_id '
+                // + 'WHERE application.app_status = $1 '
+                // + 'ORDER BY application.last_updated DESC;'
+                // + `EXECUTE filter('${req.query.filter}');`
+                // + 'DEALLOCATE filter;'
+                
+                // Query without join prior to setting up user sessions for testing
+                q += 'SET SEARCH_PATH TO sf;'
+                + 'PREPARE filter(text) AS '
+                + 'SELECT application.user_id, application.app_id, application.role, application.organisation, application.city, '
+                + 'application.country, application.app_date, application.deadline, application.description, application.app_status, '
+                + 'application.last_updated '
+                + 'FROM application '
+                + 'WHERE application.app_status = $1 '
+                + 'ORDER BY application.last_updated DESC;'
+                + `EXECUTE filter('${req.query.filter}');`
+                + 'DEALLOCATE filter;'
+                console.log(q);
+            }
+        }        
+    }    
     
     await pool
-        .query(q)
+        .query(q)        
         .then((results) => {
+            console.log(results);
+
             const statusArray = results[1].rows;
+            console.log(statusArray);
+
             if (prepared) {
                 for (let i = statusArray.length -1; i >= 0; i--) {
-                    if (statusArray[i].ptags === req.query.filter) {
+                    if (statusArray[i] === req.query.filter) {
                         filterArray.splice(i, 1);
                     }
                 }
 
-                const apps = results[3].rows
+                const apps = results[4].rows
+                console.log(apps);
+
                 if (apps.length === 0) {
                     return res.render('all-applications', {
                         title: 'Applications',
                         filter: req.query.filter,
-                        noSort: false,
+                        noFilter: false,
                         status: statusArray,
                         noApps: true
-                    })
+                    });
                 }
 
                 const appFeatures = ['role', 'organisation', 'city', 'country', 'app_date', 
                                      'deadline', 'description', 'app_status', 'last_updated'];
                 let appResults = [apps, appFeatures, ['last_updated']];
                 sanAppResults = [appResults, 'last_updated'];
+                // console.log(sanAppResults);
 
                 return res.render('all-applications', {
                     title: 'Applications',
-                    apps: sanAppResults,
+                    apps: apps,
                     filter: req.query.filter,
-                    noSort: false,
+                    noFilter: false,
                     status: statusArray
-                })
+                });
                         
             }
-            const apps = results[2].rows;
+            const apps = results[3].rows;
+            console.log(apps);
+
             const appFeatures = ['role', 'organisation', 'city', 'country', 'app_date', 
                                      'deadline', 'description', 'app_status', 'last_updated'];
             let appResults = [apps, appFeatures, ['last_updated']];
             sanAppResults = [appResults, 'last_updated'];
+            // console.log(sanAppResults);
 
             return res.render('all-applications', {
                 title: 'Applications',
-                apps: sanAppResults,
-                noSort: false,
+                apps: apps,
+                noFilter: true,
                 status: statusArray
             })            
         })
         .catch((e) => {
             console.log(e);
-        })
+        });
 })
 
 module.exports = router;
