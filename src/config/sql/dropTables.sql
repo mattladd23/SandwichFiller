c7e71b4f8faea804848e05e8c8991299c6336850
Matt <matt-jbl@hotmail.co.uk>
2022-06-23 22:56:15 +0100

    Db test

--- src/config/sql/dropTables.sql
+++ src/config/sql/dropTables.sql
@@ -3,7 +3,6 @@ SET SEARCH_PATH TO sf;
 -- Appropriate order in accordance with referential integrity
 
 DROP TABLE application;
-DROP TABLE employer;
 DROP TABLE users;
 DROP TABLE student;
 DROP TABLE staff;
