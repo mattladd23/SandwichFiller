acf0dd92d8d689caafd030eed6dd1e8bcc273cac
Matt <matt-jbl@hotmail.co.uk>
2022-07-18 14:47:29 +0100

    Access control middleware now working

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,16 +1,42 @@
 // Instantiate express and router
 const express = require('express');
+const pool = require('../config/db');
 const { checkIsAuthenticated } = require('../middleware/checkAuth');
 const router = express.Router();
 
 // Render loggedin tester page - call it user for now
-router.get('/', checkIsAuthenticated, (req, res) => {
-    // res.render('loggedin', {
-    //     title: 'Welcome!',
-    //     error: req.query.error      
-    // });
-    // Redirect to student while working on student user story
-    res.redirect('/student');
+router.get('/', checkIsAuthenticated, async (req, res) => {
+
+    userId = req.session.passport.user;
+
+    let q = 'SET SEARCH_PATH TO SF; ' +
+    'PREPARE redirectUser(bigint) AS ' +
+    'SELECT is_staff, is_admin ' +
+    'FROM users ' +
+    'WHERE user_id = $1; ' +
+    `EXECUTE redirectUser(${userId});` +
+    'DEALLOCATE redirectUser;';
+
+    console.log(q);
+
+    await pool
+        .query(q)
+        .then((results) => {
+
+            const isStaff = results[2].rows[0].is_staff;
+            const isAdmin = results[2].rows[0].is_admin;
+
+            if (isStaff && isAdmin) {
+                res.redirect('/admin');
+            } else if (isStaff && !isAdmin) {
+                res.redirect('/staff');
+            } else {
+                res.redirect('/student');
+            }
+        })
+        .catch((e) => {
+            console.log(e);
+        })
 });
 
 module.exports = router;
